845bd9e8b223ce204da14fc4b9fd6c163e7ee513
Elizabete <elizabete958@gmail.com>
2024-02-13 20:50:14 -0300

    Configuração dos cards ul

--- src/view.js
+++ src/view.js
@@ -1,6 +1,22 @@
 export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
-};
+  const ul = document.createElement('ul');
+  ul.classList.add('container__card');
 
+  data.forEach((item) => {
+    ul.innerHTML += `
+      <li itemscope itemtype="Coffee" class="container__card">
+        <div class="content__card">
+          <dl itemscope itemtype="#">
+            <dt><img src="${item.imageUrl}" alt="Imagem do Filme" itemprop="${item.name}" class="image__card" /></dt>
+            <dd itemprop="name" class="name__card">${item.name}</dd>
+
+            <dd itemprop="shortDescription" class="shortDescription">${item.shortDescription}</dd>
+            <dd itemprop="intensidadeCoffe" class="intensidadeCoffe">${item.facts.intensidadeCoffe}</dd>
+
+          </dl>
+        </div>
+      </li>
+    `;
+  });
+  return ul;
+}
